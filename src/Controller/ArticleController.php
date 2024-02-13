49e97dfe3bb79ec3e2ceb13f17f011bd0397f517
Hugo Nicolas <hugo.nicolas@les-tilleuls.coop>
2024-02-13 16:53:20 +0100

    chore: create money type reproducer for large numbers

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ArticleController extends AbstractController
+{
+    #[Route('/article', name: 'app_article', methods: ['GET', 'POST'])]
+    public function index(Request $request): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+
+            dd($article);
+        }
+
+        return $this->render('article.html.twig', ['form' => $form]);
+    }
+}
